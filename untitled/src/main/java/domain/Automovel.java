6ef88f9eaae0d153e49dc2985634a48cea57d69a
renato <1231708@isep.ipp.pt>
2025-02-26 17:12:16 +0000

    Implementação do método novaClasse da classe GrupoAutomovel.

--- untitled/src/main/java/domain/Automovel.java
+++ untitled/src/main/java/domain/Automovel.java
@@ -1,4 +1,11 @@
 package domain;
 
 public class Automovel {
+    private String nome;
+    private Integer portas;
+    private String classe;
+
+    public void novaClasse(String classe) {
+        this.classe = classe;
+    }
 }
